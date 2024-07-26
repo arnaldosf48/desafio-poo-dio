d186bed0f414ebd7dba17cc576d153c56168d41b
ArnaldoSF <arnaldo.sf48@gmail.com>
2024-07-26 12:11:31 -0300

    first commit test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descricao do curso");
+        curso1.setCargoHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso javaScript");
+        curso2.setDescricao("Descricao do curso");
+        curso2.setCargoHoraria(10);
+
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria do java");
+        mentoria1.setDescricao(" descricao mentoria");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
+
     }
 }
---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,42 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+
+    //atributos
+    private String titulo;
+    private String descricao;
+    private int cargoHoraria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargoHoraria() {
+        return cargoHoraria;
+    }
+
+    public void setCargoHoraria(int cargoHoraria) {
+        this.cargoHoraria = cargoHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargoHoraria=" + cargoHoraria +
+                '}';
+    }
+}
